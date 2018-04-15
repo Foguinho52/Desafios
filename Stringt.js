8d6656dfc09e5ba062e43cc4774262e94778a10a
Wesllen de Araújo Lima <37088281+Foguinho52@users.noreply.github.com>
2018-04-15 20:12:09 -0300

    Delete Stringt.js

--- Stringt.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-            var strReverso = "";
-            for (let i = str.length - 1; i >= 0; i--) {
-                strReverso += str[i];
-            }
-            return strReverso;
-        }
-        reverseString("abacate")
