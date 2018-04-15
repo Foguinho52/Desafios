59cd67a5fccd6d63d7f67d119f8f6d0005d037d0
Wesllen de Araújo Lima <37088281+Foguinho52@users.noreply.github.com>
2018-04-15 20:11:59 -0300

    Create Stringt.js

---
+++ Stringt.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+            var strReverso = "";
+            for (let i = str.length - 1; i >= 0; i--) {
+                strReverso += str[i];
+            }
+            return strReverso;
+        }
+        reverseString("abacate")
